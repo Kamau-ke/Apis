fce4a9c77a0fceac2427890a9600f97742fbde19
Kamau-ke <kamauwamkuu@gmail.com>
2024-06-21 09:02:00 +0300

    new update

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,17 +1,24 @@
 const express=require('express')
 const app=express()
+const errorHandler=require('./middleware/error-handler')
+
 const tasks=require('./routes/tasks')
 
 const connectDB=require('./db/connect')
 require('dotenv').config()
 const notFound=require('./middleware/not-found')
 
+
+
 const uri=process.env.URI
+
 
 app.use(express.json())
 app.use(express.static('./public'))
 app.use('/api/v1/tasks',tasks )
 app.use(notFound)
+app.use(errorHandler)
+
 var port=3000
 
 
