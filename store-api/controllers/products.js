378190daf2ab1b85dc33e428551a4be70f2614c3
Kamau-ke <kamauwamkuu@gmail.com>
2024-06-25 10:28:12 +0300

    store api

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -1,9 +1,30 @@
+const Product=require('../models/products')
+
 const getAllProductsStatic=async(req, res)=>{
+
+    const {featured, company, name}=req.query
+    const queryObject={}
+
+    if(featured){
+        queryObject.featured=featured==='true'?true: false
+    }
+
+
+    if(company){
+        queryObject.company=company
+    }
+
+    if(name){
+        queryObject.name={$regex: name, $options:'i'}
+    }
+
+    console.log(queryObject)
+    const products=await Product.find(queryObject )
     
-    res.status(200).json({msg:'all products static'})
+    return res.status(200).json({products, nbHits: products.length})
 }
 const getAllProducts=async(req, res)=>{
-    res.status(200).json({msg:'all products route'})
+   return res.status(200).json({msg:'all products route'})
 }
 
 module.exports={
