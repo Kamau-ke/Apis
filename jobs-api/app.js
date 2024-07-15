db2caf68045ab8bcbfcb021a2beb89ec9f55ff6e
Kamau-ke <kamauwamkuu@gmail.com>
2024-07-15 08:20:50 +0300

    new update

--- jobs-api/app.js
+++ jobs-api/app.js
@@ -1,6 +1,15 @@
 require('dotenv').config()
 require('express-async-errors')
 
+// security
+
+const helmet=require('helmet')
+const cors=require('cors')
+const xss=require('xss-clean')
+const rateLimiter=require('express-rate-limit')
+
+
+
 const express=require('express')
 const app=express()
 
@@ -15,6 +24,16 @@ const notFoundMiddleware=require('./middleware/not-found')
 const errorHandler=require('./middleware/error-handler')
 
 app.use(express.json())
+
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false,
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs',authenticateUser, jobsRouter)
--- jobs-api/middleware/error-handler.js
+++ jobs-api/middleware/error-handler.js
@@ -1,12 +1,36 @@
 const {customApiError}=require('../errors')
 const {StatusCodes}=require('http-status-codes')
+
 const errorHandler=(err, req, res, next)=>{
-    if(err instanceof customApiError){
-        return res.status(err.statusCode).json({msg:err.message})
+    let customError={
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg:err.message || 'Something went wrong please try again later'
     }
+
+ 
+    // if(err instanceof customApiError){
+    //     return res.status(err.statusCode).json({msg:err.message})
+    // }
+
+    if(err.name=== 'ValidationError'){
+        customError.msg=Object.values(err.errors).map(item=>item.message).join(',')
+        customError.statusCode=400
+    }
+
+    if(err.name=== 'CastError'){
+        customError.msg=`No item found with id ${err.value}`
+        customError.statusCode=404
+    }
+    
+
+    if(err.code && err.code=== 11000){
+        customError.msg=`Duplicate value for ${Object.keys(err.keyValue)} field, please choose another value`
+        customError.statusCode=400
+    }
+    
   
 
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: 'Something went wrong, please try again later'})
+    res.status(customError.statusCode).json({msg: customError.msg})
 
 }
 
