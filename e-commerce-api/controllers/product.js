1767beb03e0e2ae53a866a330f46fefc9deb5ec7
Kamau-ke <kamauwamkuu@gmail.com>
2024-08-31 09:09:06 +0300

    new update

--- e-commerce-api/controllers/product.js
+++ e-commerce-api/controllers/product.js
@@ -1,21 +1,50 @@
-const createProduct=(req, res)=>{
-    res.send('create product')
+const Product=require('../models/products')
+const {StatusCodes}=require('http-status-codes')
+const badRequest=require('../errors/badRequest')
+
+const createProduct=async (req, res)=>{
+    const product =await Product.create({...req.body})
+    res.status(StatusCodes.OK).json({product})
 }
 
-const viewProducts=(req, res)=>{
-    res.send('view products')
+const viewProducts=async (req, res)=>{
+    const products=await Product.find({})
+
+    res.status(StatusCodes.OK).json({products, count: products.length})
+
 }
 
-const viewProduct=(req, res)=>{
-    res.send('view product')
+const viewProduct=async (req, res)=>{
+    const {id:productId}=req.params
+    const product=await Product.find({_id:productId})
+
+    if(!product){
+        throw new badRequest('No item found with that id')
+    }
+
+    res.status(StatusCodes.OK).json({product})
 }
 
-const editProduct=(req, res)=>{
-    res.send('edit product')
+const editProduct=async (req, res)=>{
+    const {id:productId}=req.params
+    const product=await Product.find({_id:productId})
+    if(!product){
+        throw new badRequest('Product not found')
+    }
+    const newProduct=await Product.findOneAndUpdate({_id:productId},req.body, {runValidators:true, new:true})
+
+    res.status(StatusCodes.OK).json({newProduct})
 }
 
-const deleteProduct=(req, res)=>{
-    res.send('delete product')
+const deleteProduct=async (req, res)=>{
+    const {id:productId}=req.params
+    const product=await Product.find({_id})
+    if(!product){
+        throw new badRequest('product not found')
+    }
+
+    await Product.findOneAndDelete({_id:productId})
+    res.status(StatusCodes.OK).json({msg: "product deleted successfully"})
 }
 
 
