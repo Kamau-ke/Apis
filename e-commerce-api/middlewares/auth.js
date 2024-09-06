3af7e5a36e9c6533234661e63a7ae2345e1b87ba
Kamau-ke <kamauwamkuu@gmail.com>
2024-09-06 22:30:44 +0300

    new update

--- e-commerce-api/middlewares/auth.js
+++ e-commerce-api/middlewares/auth.js
@@ -1,3 +1,35 @@
-const express=require('express')
-const jwt=require('jsonwebtoken')
+const unAuthorized=require('../errors/unAuthorized')
+const {isValidToken}=require('../utils/jwt')
 
+const authenticateUser=async (req, res, next)=>{
+    const token=req.signedCookies.token
+
+    if(!token){
+        throw new unAuthorized('Authentication invalid')
+    }
+
+    try {
+
+        const {name, id, role}=isValidToken({token})
+        req.user={name, id, role}
+
+        next()
+        
+    } catch (error) {
+        throw new unAuthorized('Authentication invalid')
+    }
+}
+
+const authorizeRole= (...roles)=>{
+    return(req, res, next)=>{
+        if(!roles){
+            throw new unAuthorized('un authorized to access this route')
+        }
+        next()
+    }
+}
+
+
+module.exports={
+    authenticateUser, authorizeRole
+}
